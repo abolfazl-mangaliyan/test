7d3fc5b6cb18452fa1cf06bfaaea8543c92807a2
“[Abolfazl <[abolfazl.mangaliyan.sh@gmail.com]>
2024-10-29 12:17:22 +0330

    test pj

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="form">
+        <h2>iam groot</h2>
+          <form action="#">
+            <input placeholder='enter your name' type="text" />
+            <button>enter</button>
+            <input type="text" placeholder="show name" />
+          </form>
+        </div>  
+    </div>
+  );
+}
+
+export default App;
